6830c8aac91d58ef0899e1c85c52334035a74c8c
Kre0ns <yonpun13@gmail.com>
2026-09-17 14:00:56 +0300

    feat: basic cpu definition

--- src/gb.h
+++ src/gb.h
@@ -6,6 +6,60 @@
 
 typedef struct
 {
+    union
+    {
+        struct
+        {
+            uint8_t f;
+            uint8_t a;
+        };
+        
+        uint16_t af;
+    };
+
+    union
+    {
+        struct
+        {
+            uint8_t c;
+            uint8_t b;
+        };
+        
+        uint16_t bc;
+    };
+
+    union
+    {
+        struct
+        {
+            uint8_t e;
+            uint8_t d;
+        };
+        
+        uint16_t de;
+    };
+
+    union
+    {
+        struct
+        {
+            uint8_t l;
+            uint8_t h;
+        };
+        
+        uint16_t hl;
+    };
+
+    uint16_t sp;
+    uint16_t pc;
+
+    bool ime;
+    bool halted;
+    
+} cpu_t;
+
+typedef struct
+{
     uint8_t *rom;
     size_t rom_size;
 
@@ -15,6 +69,7 @@ typedef struct
 typedef struct
 {
     cart_t cart;
+    cpu_t cpu;
     
     uint8_t wram[0x2000];
     uint8_t vram[0x2000];
